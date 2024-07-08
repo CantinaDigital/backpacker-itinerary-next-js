f7ed03b0939e9febbee0da32ec92a4fd6fe7f72c
Cantina Digital <9746028+CantinaDigital@users.noreply.github.com>
2024-07-07 20:02:33 -0600

    Created Index.js

---
+++ app/pages/index.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import HomePage from '../components/homePage';
+import Itinerary from '../components/itinerary';
+
+const Home = () => {
+  const [itinerary, setItinerary] = useState(null);
+
+  const handleSearch = (data) => {
+    setItinerary(data);
+  };
+
+  return (
+    <div>
+      <HomePage onSearch={handleSearch} />
+      <Itinerary itinerary={itinerary} />
+    </div>
+  );
+};
+
+export default Home;
